1ecb2afa05926ee80723a51ac00b9404c79d5b4e
Alexceptiion <alex_rama@hotmail.fr>
2021-06-07 14:57:37 +0200

    maj

--- reactjs/src/components/jumbotron/styles/jumbotron.js
+++ reactjs/src/components/jumbotron/styles/jumbotron.js
@@ -1,10 +1,18 @@
 import styled from "styled-components/macro";
 
+export const Item = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+  overflow: hidden;
+`;
+
 export const Inner = styled.div`
   display: flex;
   align-items: center;
-  flex-direction: ${({ direction }) => direction};
   justify-content: space between;
+  flex-direction: ${({ direction }) => direction};
   max-width: 1100px;
   margin: auto;
   width: 100%;
@@ -16,6 +24,12 @@ export const Inner = styled.div`
 
 export const Pane = styled.div`
   width: 50%;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    padding: 0 45px;
+    text-align: center;
+  }
 `;
 
 export const Title = styled.h1`
