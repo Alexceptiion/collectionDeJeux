9672a218fbc7b3929a88ec988275e164666bb042
Alexceptiion <alex_rama@hotmail.fr>
2021-06-08 10:31:33 +0200

    maj

--- reactjs/src/components/jumbotron/styles/jumbotron.js
+++ reactjs/src/components/jumbotron/styles/jumbotron.js
@@ -1,17 +1,9 @@
 import styled from "styled-components/macro";
 
-export const Item = styled.div`
-  display: flex;
-  border-bottom: 8px solid #222;
-  padding: 50px 5%;
-  color: white;
-  overflow: hidden;
-`;
-
 export const Inner = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space between;
+  justify-content: space-between;
   flex-direction: ${({ direction }) => direction};
   max-width: 1100px;
   margin: auto;
@@ -41,19 +33,30 @@ export const Title = styled.h1`
     font-size: 35px;
   }
 `;
+
 export const SubTitle = styled.h2`
   font-size: 26px;
   font-weight: normal;
   line-height: normal;
 
-  @meadia (max-width: 600px) {
+  @media (max-width: 600px) {
     font-size: 18px;
   }
 `;
+
 export const Image = styled.img`
   max-width: 100%;
   height: auto;
 `;
+
+export const Item = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+  overflow: hidden;
+`;
+
 export const Container = styled.div`
   @media (max-width: 1000px) {
     ${Item}:last-of-type h2 {
