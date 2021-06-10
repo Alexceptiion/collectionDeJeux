9878d5d7daaab00996ee83ee50f8d30170d1c735
Alexceptiion <alex_rama@hotmail.fr>
2021-06-10 15:55:51 +0200

    maj

--- reactjs/src/app.js
+++ reactjs/src/app.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
 
-export default function App() {
+export function App() {
   return (
     <Router>
       <Route exact path="/browse">
--- reactjs/src/lib/firebase.prod.js
+++ reactjs/src/lib/firebase.prod.js
@@ -1,24 +1,53 @@
 import Firebase from "firebase/app";
-import "Firebase/firestore";
-import "Firebase/auth";
+import "firebase/firestore";
+import "firebase/auth";
 
-//nous devons en quelque sorte semer la base de données
-
-//nous avons besoin de la configuration ici
+// 1) when seeding the database you'll have to uncomment this!
+// import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyACT87J2nycNqH1Gyv5ReNarVPTFQ4kzXU",
+  apiKey: "AIzaSyA2fw9E1Jrih9-Dxwc1OEqXAde3YjpjjAo",
   authDomain: "origo-59dc3.firebaseapp.com",
+  databaseURL: "https://origo-59dc3.firebaseio.com",
   projectId: "origo-59dc3",
   storageBucket: "origo-59dc3.appspot.com",
   messagingSenderId: "398516317755",
   appId: "1:398516317755:web:919b624d6707d7032f761e",
-  measurementId: "G-VP9XPB61BX",
+  // measurementId: "G-VP9XPB61BX",
 };
 
 const firebase = Firebase.initializeApp(config);
+// 2) when seeding the database you'll have to uncomment this!
+// seedDatabase(firebase);
+// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
+
+// import Firebase from "firebase/app";
+// import "firebase/firestore";
+// import "firebase/auth";
+// import { seedDataBase } from "../seed";
+
+// //nous devons en quelque sorte semer la base de données
+
+// //nous avons besoin de la configuration ici
+
+// const config = {
+//   apiKey: "AIzaSyA2fw9E1Jrih9-Dxwc1OEqXAde3YjpjjAo",
+//   authDomain: "origo-59dc3.firebaseapp.com",
+//   databaseURL: "https://origo-59dc3.firebaseio.com",
+//   projectId: "origo-59dc3",
+//   storageBucket: "origo-59dc3.appspot.com",
+//   messagingSenderId: "398516317755",
+//   appId: "1:398516317755:web:919b624d6707d7032f761e",
+//   //measurementId: "G-VP9XPB61BX",
+// };
+
+// const firebase = Firebase.initializeApp(config);
+
+// seedDataBase(firebase);
+
+// export { firebase };
 
 // <!-- The core Firebase JS SDK is always required and must be listed first -->
 // <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js"></script>
